ae0db97462e786e127883fa4ec41d5b758dd1ba7
Аlex Kukhtin <alex.kukhtin@outlook.com>
2022-02-01 13:12:38 +0200

    net 6.0

--- Startup.cs
+++ Startup.cs
@@ -4,27 +4,26 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace A2v10.Core.Sample
+namespace A2v10.Core.Sample;
+public class Startup
 {
-	public class Startup
+	public Startup(IConfiguration configuration)
 	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+		Configuration = configuration;
+	}
 
-		public IConfiguration Configuration { get; }
+	public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.UsePlatform(Configuration);
-		}
+	// This method gets called by the runtime. Use this method to add services to the container.
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.UsePlatform(Configuration);
+	}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			app.ConfigurePlatform(env);
-		}
+	// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+	{
+		app.ConfigurePlatform(env);
 	}
 }
+
